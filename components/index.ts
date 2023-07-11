3577b6ef9ab205b3e12297dac9febac203d319e5
Alexis Novas <47201459+alexisnovas@users.noreply.github.com>
2023-07-11 13:38:54 -0400

    added layout

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,11 @@
 import Hero from './Hero'
 import CustomButton from './CustomButton'
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export {
     Hero,
-    CustomButton
+    CustomButton,
+    Navbar,
+    Footer
 }
--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,42 @@
 import { MouseEventHandler } from "react";
 
+export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
+}
+
+export interface FilterProps {
+    manufacturer?: string;
+    year?: number;
+    model?: string;
+    limit?: number;
+    fuel?: string;
+}
+
+export interface HomeProps {
+    searchParams: FilterProps;
+}
+
+export interface CarCardProps {
+    model: string;
+    make: string;
+    mpg: number;
+    transmission: string;
+    year: number;
+    drive: string;
+    cityMPG: number;
+}
+
 export interface CustomButtonProps {
     isDisabled?: boolean;
     btnType?: "button" | "submit";
@@ -8,4 +45,24 @@ export interface CustomButtonProps {
     title: string;
     rightIcon?: string;
     handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface OptionProps {
+    title: string;
+    value: string;
+}
+
+export interface CustomFilterProps {
+    title: string;
+    options: OptionProps[];
+}
+
+export interface ShowMoreProps {
+    pageNumber: number;
+    isNext: boolean;
+}
+
+export interface SearchManuFacturerProps {
+    manufacturer: string;
+    setManuFacturer: (manufacturer: string) => void;
 }
